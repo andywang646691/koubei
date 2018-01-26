8181c39a20d4dbb795af57fdedea585f8f7ec208
jackie <jackie.zheng@cardinfolink.com>
2018-01-26 16:33:04 +0800

    路由未匹配时 重定向

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,27 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 const Home = r => require.ensure([], () => r(require('@/views/home/Home')), 'Home')
+const discount1 = r => require.ensure([], () => r(require('@/views/coupous/discount1')), 'discount1')
 
-export default new Router({
+let router = new Router({
   routes: [
+    {
+      path: '',
+      component: Home
+    },
     {
       path: '/',
       component: Home
+    },
+    {
+      path: '/discount1',
+      component: discount1
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
+
+export default router
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,32 +3,18 @@
     top-head
     Coupous.row-gap
     Activity.row-gap
-    .cell-wrapper
-      form-cell(
-        title="标题文字"
-        v-model="cellValue"
-      )
-      mt-cell(
-      title="必填"
-      value="我就是不填"
-      )
-      File-Uploader
 </template>
 
 <script>
 import Coupous from './children/Coupons.vue'
 import Activity from './children/Activity.vue'
 import TopHead from '@/components/TopHead.vue'
-import FormCell from '@/components/FormCell.vue'
-import FileUploader from '@/components/FileUploader.vue'
 export default {
   name: 'home',
   components: {
     Coupous,
     Activity,
-    TopHead,
-    FormCell,
-    FileUploader
+    TopHead
   },
   data () {
     return {
