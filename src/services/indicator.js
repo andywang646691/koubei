9f8f5fd78e6e3afcc2819666b9f5602efb687e67
jackie <jackie.zheng@cardinfolink.com>
2018-03-05 16:36:34 +0800

    add: loading列表增加2个请求

--- src/services/indicator.js
+++ src/services/indicator.js
@@ -1,5 +1,7 @@
 const indicatorApis = [
-  '/neuStar/alp/alpCreateCampaign.json'
+  '/neuStar/alp/alpCreateCampaign.json',
+  '/neuStar/alp/refreshShop.json',
+  '/neuStar/alp/getAllShops.json'
 ]
 
 export default indicatorApis
